16ab0af8e3059147bfdd09db20c8e6ec160f9410
Mark Abernathy <mark.abernathy@inin.com>
2016-06-22 20:15:13 -0400

    Save stuff commit; added placeholder dice upgrade store UI

--- app/services/player.js
+++ app/services/player.js
@@ -3,6 +3,32 @@ import Ember from 'ember';
 export default Ember.Service.extend({
   game: Ember.inject.service('game'),
   player_money: 0,
+  player_dice: [
+    {
+      dice_school : 'life',
+      neutral_sides : 1,
+      school_sides : 1,
+      crit_sides : 1
+    },
+    {
+      dice_school : 'phys',
+      neutral_sides : 1,
+      school_sides : 1,
+      crit_sides : 1
+    },
+    {
+      dice_school : 'illusion',
+      neutral_sides : 1,
+      school_sides : 1,
+      crit_sides : 1
+    },
+    {
+      dice_school : 'death',
+      neutral_sides : 1,
+      school_sides : 1,
+      crit_sides : 1
+    },
+  ],
   isBattling: true,
 
   endBattlePhase() {

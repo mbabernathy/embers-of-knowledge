0ebd06194e919a92f49d249a80b6a2184f337f04
Mark Abernathy <mbabernathy@gmail.com>
2018-10-06 13:43:04 -0400

    Upgrade bootstrap dependency, add production build to docs

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -4,8 +4,8 @@ const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 module.exports = function(defaults) {
   var app = new EmberApp(defaults, {
     'ember-bootstrap': {
-      'bootstrapVersion': 3,
-      'importBootstrapFont': true,
+      'bootstrapVersion': 4,
+      'importBootstrapFont': false,
       'importBootstrapCSS': true
     }
   });

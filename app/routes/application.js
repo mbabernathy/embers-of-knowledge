c045d52d860caff9d50afc0a055b961b7dd648f9
Mark Abernathy <mbabernathy@gmail.com>
2018-10-05 20:40:14 -0400

    Add saving playerstate to localstorage

--- app/routes/application.js
+++ app/routes/application.js
@@ -5,6 +5,7 @@ export default Ember.Route.extend({
   player: Ember.inject.service('player'),
 
   model() {
+    this.get('player').loadPlayer();
     return { player: this.get('player') };
   }
 });
--- app/services/player.js
+++ app/services/player.js
@@ -3,42 +3,69 @@ import Ember from 'ember';
 export default Ember.Service.extend({
   game: Ember.inject.service('game'),
   stats: Ember.inject.service('stats'),
-  max_life: 5,
+  max_life: 0,
   player_money: 0,
-  player_dice: [
-    Ember.Object.create({
-      dice_school : 'life',
-      neutral_sides : 1,
-      school_sides : 1,
-      crit_sides : 1
-    }),
-    Ember.Object.create({
-      dice_school : 'phys',
-      neutral_sides : 1,
-      school_sides : 1,
-      crit_sides : 1
-    }),
-    Ember.Object.create({
-      dice_school : 'illusion',
-      neutral_sides : 1,
-      school_sides : 1,
-      crit_sides : 1
-    }),
-    Ember.Object.create({
-      dice_school : 'death',
-      neutral_sides : 1,
-      school_sides : 1,
-      crit_sides : 1
-    }),
-  ],
-  knownSpells: ['0-00', '1-00', '3-00', '5-00', '7-00'],
-  isBattling: true,
+  player_dice: [],
+  knownSpells: [],
+  isBattling: false,
+
+  resetDefaultPlayer() {
+    this.set('max_life', 5);
+    this.set('player_money', 0);
+    this.set('knownSpells', ['0-00', '1-00', '3-00', '5-00', '7-00']);
+    let playerDice = [];
+    ['life','phys','illusion','death'].forEach((schoolString) => {
+      playerDice.pushObject(Ember.Object.create({
+        dice_school : schoolString,
+        neutral_sides : 1,
+        school_sides : 1,
+        crit_sides : 1
+      }))
+    });
+    this.set('player_dice', playerDice);
+    this.set('isBattling', true);
+  },
+
+  loadPlayer() {
+    try {
+      let playerObject = JSON.parse(window.localStorage.getItem('player'));
+      if (!playerObject) {
+        throw "loading playerstate error";
+      }
+      this.set('max_life', playerObject.max_life);
+      this.set('player_money', playerObject.player_money);
+      this.set('knownSpells', playerObject.knownSpells);
+      let playerDice = [];
+      playerObject.player_dice.forEach((dice) => {
+        playerDice.pushObject(Ember.Object.create({
+          dice_school : dice.dice_school,
+          neutral_sides : dice.neutral_sides,
+          school_sides : dice.school_sides,
+          crit_sides : dice.crit_sides
+        }));
+      });
+      this.set('player_dice', playerDice);
+      this.set('isBattling', false);
+    } catch (e) {
+      this.resetDefaultPlayer();
+    }
+  },
+  savePlayer() {
+    let playerObject = {
+      max_life: this.get('max_life'),
+      player_money: this.get('player_money'),
+      knownSpells: this.get('knownSpells'),
+      player_dice: this.get('player_dice')
+    };
+    window.localStorage.setItem('player', JSON.stringify(playerObject));
+  },
 
   endBattlePhase() {
     this.set('player_money', this.get('player_money') + this.get('stats.expectedIncome'));
 
     // Switch out of battleframe
     this.set('isBattling', false);
+    this.savePlayer();
   },
 
   startNewBattle() {

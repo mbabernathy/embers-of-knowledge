4993d6f726467ded91a573ff273996cdb5b47577
Mark Abernathy <mbabernathy@gmail.com>
2018-10-06 19:54:53 -0400

    Really fix prod app build config

--- config/environment.js
+++ config/environment.js
@@ -43,7 +43,8 @@ module.exports = function(environment) {
   }
 
   if (environment === 'production') {
-    rootURL = '/embers-of-knowledge'
+    ENV.rootURL = '/embers-of-knowledge';
+    ENV.locationType = 'hash';
   }
 
   return ENV;
